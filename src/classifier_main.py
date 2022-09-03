ade60ad795d3055ae39b28335e15115466ea702a
Oluyemi Amujo <oluyemiamujo@gmail.com>
2022-09-03 14:48:28 +0100

    second commit

--- src/classifier_main.py
+++ src/classifier_main.py
@@ -18,33 +18,35 @@ def get_recommendations(N, res_class):
     # order the scores with and filter to get the highest N scores
 #     new_scores = [x for x in scores if x > 0.5 ]
     #print(new_scores)
-    top = sorted(range(len(res_class)), key=lambda i: res_class[i],  reverse=True)[:5]
-    print(len(top))
+    df_data = df_recipes.loc[df_recipes['food types'] == res_class[0]].sample(n = 10)
+
+    #top = sorted(range(len(df_data)), key=lambda i: df_data[i],  reverse=True)
+    #print(len(top))
     # create dataframe to load in recommendations 
     recommendation = pd.DataFrame(columns= ['id', 'recipe_name', 'cook_time', 'n_steps', 'steps', 'n_ingredients', 'calories', 'total fat (PDV)', 'sugar (PDV)', 'sodium (PDV)', 'protein (PDV)', 'saturated fat (PDV)', 'carbohydrates (PDV)', 'food types'])
     count = 0
-    for i in top:
-        recommendation.at[count, "id"] = df_recipes["id"][i]
-        recommendation.at[count, "recipe_name"] = title_parser(df_recipes["name"][i])
-        recommendation.at[count, "cook_time"] = df_recipes["minutes"][i]        
-        recommendation.at[count, "n_steps"] = df_recipes["n_steps"][i]
-        recommendation.at[count, "steps"] = df_recipes["steps"][i]
-        recommendation.at[count, "description"] = df_recipes["description"][i]        
-        recommendation.at[count, "ingredients"] = ingredient_parser_final(
-            df_recipes["ingredients"][i]
-        )
-        recommendation.at[count, "n_ingredients"] = df_recipes["n_ingredients"][i]
-        recommendation.at[count, "calories"] = df_recipes["calories"][i]
-        recommendation.at[count, "total fat (PDV)"] = df_recipes["total fat (PDV)"][i]
-        recommendation.at[count, "sugar (PDV)"] = df_recipes["sugar (PDV)"][i]
-        recommendation.at[count, "sodium (PDV)"] = df_recipes["sodium (PDV)"][i]
-        recommendation.at[count, "protein (PDV)"] = df_recipes["protein (PDV)"][i]
-        recommendation.at[count, "saturated fat (PDV)"] = df_recipes["saturated fat (PDV)"][i]
-        recommendation.at[count, "carbohydrates (PDV)"] = df_recipes["carbohydrates (PDV)"][i]
-        recommendation.at[count, "food types"] = df_recipes["food types"][i]
+    #for i in df_data:
+        #recommendation.at[count, "id"] = df_recipes["id"][i]
+        #recommendation.at[count, "recipe_name"] = title_parser(df_recipes["name"][i])
+        #recommendation.at[count, "cook_time"] = df_recipes["minutes"][i]        
+        #recommendation.at[count, "n_steps"] = df_recipes["n_steps"][i]
+        #recommendation.at[count, "steps"] = df_recipes["steps"][i]
+        #recommendation.at[count, "description"] = df_recipes["description"][i]        
+        #recommendation.at[count, "ingredients"] = ingredient_parser_final(
+            #df_recipes["ingredients"][i]
+        #)
+        #recommendation.at[count, "n_ingredients"] = df_recipes["n_ingredients"][i]
+        #recommendation.at[count, "calories"] = df_recipes["calories"][i]
+        #recommendation.at[count, "total fat (PDV)"] = df_recipes["total fat (PDV)"][i]
+        #recommendation.at[count, "sugar (PDV)"] = df_recipes["sugar (PDV)"][i]
+        #recommendation.at[count, "sodium (PDV)"] = df_recipes["sodium (PDV)"][i]
+        #recommendation.at[count, "protein (PDV)"] = df_recipes["protein (PDV)"][i]
+        #recommendation.at[count, "saturated fat (PDV)"] = df_recipes["saturated fat (PDV)"][i]
+        #recommendation.at[count, "carbohydrates (PDV)"] = df_recipes["carbohydrates (PDV)"][i]
+        #recommendation.at[count, "food types"] = df_recipes["food types"][i]
 #         recommendation.at[count, "score"] = f"{new_scores[i]}"
-        count += 1
-    return recommendation
+        #count += 1
+    return df_data
 
 # neaten the ingredients being outputted 
 def ingredient_parser_final(ingredient):
@@ -82,7 +84,7 @@ def RecSys(inputs, N=5):
 
 if __name__ == "__main__":
     # test ingredients
-    inputs = [[5, 6, 5, 8.2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
+    inputs = [[5, 6, 5, 8.2, 0.0, 10.0, 2.0, 0.0, 0.0, 0.0]]
     recs = RecSys(inputs)
     print(recs)
     #print(scores)
--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
 PARSED_PATH_MINI = "../input/PARSED_recipes_mini.csv"
 TFIDF_ENCODING_PATH = "../input/tfidf_encodings.pkl"
 TFIDF_MODEL_PATH = "../models/tfidf.pkl"
+
+CLASSIFIER_MODEL_PATH = "../models/classifier.pkl"
